07f4aaab07cc3f2ff5c97a01a791d83b52fe2815
joyo\xjoyo <xjoyo@outlook.com>
2026-04-18 22:26:26 +0800

    ok

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -6,6 +6,6 @@ interface Env {
   export const onRequest: PagesFunction<Env> = async (context) => {
     const { request, env } = context;
     const url = new URL(request.url);
-    const target = new URL(url.pathname + url.search, env.API_ORIGIN);
+    const target = new URL(url.pathname + url.search, "https://api.cheapstoken.ai");
     return fetch(target.toString(), request);
   };
